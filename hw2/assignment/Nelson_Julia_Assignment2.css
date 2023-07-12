3b13fee09d92e9be7087426dd96b12f3a5df0a0f
Julia Nelson <43449670+jnelson6@users.noreply.github.com>
2023-07-12 11:53:36 -0500

    Add files via upload

---
+++ hw2/assignment/Nelson_Julia_Assignment2.css
@@ -0,0 +1,73 @@
+/* Put your solutions to the assignment here */
+body {background-color: #eee;}
+
+header {background-color: #49516f;
+		display: flex;
+		align-items: center;
+		padding-top: 20px;
+		padding-bottom: 20px;}
+
+header h1 {margin: 0px;
+			color: white;}
+
+header span {margin:0px;
+			color: white;}
+
+.logo {padding: 5px;
+		margin: 10px;
+		height: 100px;
+		width: 100px;
+		border-width: 5px;
+		color: white;
+		border-style:solid;}
+
+.posts {display: flex;
+		flex-wrap: wrap;}
+
+.post {flex-grow: inherit;}
+
+.post-body {height: 200px;}
+
+.post-container {margin: 10px;
+			padding: 10px;
+			background-color: white;
+			display: flex;
+			align-items: center;}
+
+.post-pic {width: 200px;
+			height: 200px;
+			margin-right: 10px;}
+
+@media screen and (min-width: 999px) {
+	.posts{flex-direction: column;
+			justify-content: center;
+			align-items: center;}
+	.post{max-width: 800px;
+			flex: inherit;}	
+	}
+			
+@media screen and (max-width: 998px) and (min-width: 400px) {
+	header{justify-content: center;}
+	.posts{display:flex;
+			flex-direction: row;
+			flex-wrap: wrap;}
+	.post-container{flex-direction: column;}
+	.post{flex: 50%;
+			max-width: 800px;}
+	.post-pic{width: 100%;
+				height: auto;}
+}
+
+@media screen and (max-width: 399px) {
+	header{display: flex;
+			flex-direction: column;
+			text-align: center;}
+	.logo{border-radius: 50%;}
+	.posts{flex-basis: auto;
+			flex-direction: column;}
+	.post-container{flex-direction: column;}
+	.post-pic{width: 100%;
+				height: auto;}
+}
+
+
